e35d6a049fec1975dbb293f7506345b8a1b9051c
Ayush Dwivedi <dwivediayush028@gmail.com>
2025-02-18 23:23:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
-const daysOfAYear = (year)=>{
-	if(year%4===0)
-	{
-		if(year%100===4)
-		{
-			if(year%400===0)
-			{
-				return 366;
-			}
-			else
-			{
-				return 365;
-			}
-		}
-		else
-		{
-			return 365;
-		}
-	}
-}
+const daysOfAYear = (year) => {
+    if (year % 4 === 0) {
+        if (year % 100 === 0) {
+            if (year % 400 === 0) {
+                return 366; 
+            } else {
+                return 365; 
+            }
+        } else {
+            return 366; 
+        }
+    } else {
+        return 365; 
+    }
+};
